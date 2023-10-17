1d9943f91ce67e43d46779e838df686a58df6aba
Paul <impetus@yandex.ru>
2023-10-17 18:49:08 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,78 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import java.lang.Math.sqrt
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var editA: EditText
+    private lateinit var editB: EditText
+    private lateinit var editC: EditText
+    private lateinit var btn: Button
+    private lateinit var res: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        editA = findViewById(R.id.editTextA)
+        editB = findViewById(R.id.editTextB)
+        editC = findViewById(R.id.editTextC)
+        btn = findViewById(R.id.button)
+        res = findViewById(R.id.textView)
+
+        btn.setOnClickListener {
+            val a = editA.text.toString().toDouble()
+            val b = editB.text.toString().toDouble()
+            val c = editC.text.toString().toDouble()
+
+            val d = b*b - 4*a*c
+            when {
+                d == 0.0 -> {
+                    val x = -b/(2*a)
+                    res.text = getString(R.string.x, x.toString())
+                }
+                d < 0 -> {
+                    res.text = getString(R.string.not_solve)
+                }
+                else -> {
+                    val x1 = -b + sqrt(d)/(2*a)
+                    val x2 = -b - sqrt(d)/(2*a)
+                    res.text = getString(R.string.x1_x2,x1.toString(), x2.toString())
+                }
+            }
+
+        }
+
+
+
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+}
